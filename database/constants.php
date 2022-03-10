64439900411b00de492f16043376dc240ff8f0e0
tamanna21 <tamannamozumder21@gmail.com>
2022-03-10 14:45:09 +0600

    database

---
+++ database/constants.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+define("HOST","localhost");
+define("USER","root");
+define("PASS","");
+define("DB","inv_project");
+
+define("DOMAIN","http://localhost/inv_project/public_html");
+
+?>
